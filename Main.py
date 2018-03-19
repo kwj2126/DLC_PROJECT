bed364c8395028eec7b992ff8078e5c54ed39dcc
2016025250 <2016025250@hanyang.ac.kr>
2018-03-19 16:52:01 +0900

    bug fix

--- Main.py
+++ Main.py
@@ -18,6 +18,8 @@ class GUI(QMainWindow, form):
         code_list = self.kiwoom.get_code_list(0)  # 코스피
         code_list += self.kiwoom.get_code_list(10)  # 코스닥
 
+        screen_no = 1000
+        reg_cnt = 0
         for code in code_list:  # 호가정보 실시간 등록
             fid_list = ''
             for i in range(41, 81):
@@ -25,7 +27,12 @@ class GUI(QMainWindow, form):
                     fid_list += ';'
                 fid_list += str(i)
 
-            self.kiwoom.set_real_reg(1000, code, fid_list, 1)
+            reg_cnt += 1
+            if reg_cnt == 100:
+                reg_cnt = 0
+                screen_no += 1
+
+            self.kiwoom.set_real_reg(screen_no, code, fid_list, 1)
 
 
 if __name__ == '__main__':
